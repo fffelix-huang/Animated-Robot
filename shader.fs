079418b0cfc498b55276fbf93387fe0145421e79
Ting-Hsuan Huang <felixhuang07@gmail.com>
2025-01-26 19:51:42 +0800

    Remove unused variable in shader

--- shader.fs
+++ shader.fs
@@ -1,15 +1,10 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 ourColor;
 in vec2 TexCoord;
 
-uniform float alpha;
-
 uniform sampler2D texture1;
-uniform sampler2D texture2;
 
 void main() {
-    // FragColor = vec4(ourColor * alpha, 1.0f);
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2f);
+    FragColor = texture(texture1, TexCoord);
 }
